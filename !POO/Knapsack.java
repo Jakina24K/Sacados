cb0ff96a11adc460689ab63dfabd0b5699f99573
unknown <jakinaandriantsoa@gmail.com>
2023-09-15 15:57:32 +0300

    beta

---
+++ !POO/Knapsack.java
@@ -0,0 +1,40 @@
+/*
+    dans ce code                                dans le cours
+    - k[n][w]                                    - V(T)
+    - profit[]                                   - V($)
+    - weight[]                                   - P(k)
+    - W                                          - K
+    - n                                          - O(n)
+
+*/
+class Knapsack{
+    static int max(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+    static int knapSack(int W, int wt[], int val[], int n)
+    {
+        int i, w;
+        int K[][] = new int[n + 1][W + 1];
+
+        for (i = 0; i <= n; i++){
+            for (w = 0; w <= W; w++){
+                if(i ==0 ||  w == 0)
+                    K[i][w] = 0;
+                else if(wt[i - 1] <= w)
+                    K[i][w] = max(val[i -1] + K[i - 1][w - wt[i - 1]],K[i - 1][w]);
+                else   
+                    K[i][w] = K[i - 1][w];
+            }
+        }
+        return K[n][W];                         //Valeur maximale ; V(T) dans le cours
+    }
+    public static void main(String args[]){
+        int profit[] = new int[]{60,100,120};   //Valeur des objets ; V($) dans le cours
+        int weight[] = new int[]{10,20,30};     //Poids des objets ; P(k) dans le cours
+        int W = 50;                             //poids maximum supporter par le sac ; K dans le cours
+        int n = profit.length;                  // Nombre d'objet ; O(n) dans le cours
+        System.out.println(knapSack(W, weight, profit, n));
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+CLASSE : IGGLIA 3
+Nom et Numero :
+- LAZAHARIVELO Jakina Andriantsoa N`27; 
+- RANDRIANAVALONA Mahefa Nirina N`33;
+- ANDRITIANA FANORENANTSOA Steddi Karen N`56;
+- ANDRIANANDRASANA Finiaina N`29;
+- ANDRIANJOHANY Liantsoa Nomban'ny Avo N`14;
